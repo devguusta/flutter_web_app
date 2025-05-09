d42a19f77d08b2db65d91ccc1b572c73b503ac1a
Gustavo Silva <gustavocsil60@gmail.com>
2025-05-09 20:40:49 -0300

    refactor: remove Firebase remote config implementation and related interfaces

--- lib/core/setup/core_setup.dart
+++ lib/core/setup/core_setup.dart
@@ -1,23 +1,12 @@
-import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter_web_app/core/getit/getit_instance.dart';
 import 'package:flutter_web_app/core/http/http_client.dart';
 import 'package:flutter_web_app/core/http/http_client_implementation.dart';
-import 'package:flutter_web_app/core/remote_config/firebase_remote_config_impl.dart';
-import 'package:flutter_web_app/core/remote_config/remote_config.dart';
 
 /// This class is responsible for setting up the core dependencies of the application.
 /// It uses the `get_it` package to register the dependencies.
 /// The `setup` method is called in the `main.dart` file to initialize the dependencies.
 sealed class CoreSetup {
   static void setup() {
-    /// remote config
-
-    getIt.registerSingleton<RemoteConfig>(
-      FirebaseRemoteConfigImpl(
-        remoteConfig: FirebaseRemoteConfig.instance,
-      ),
-    );
-
     /// HTTP CLIENT
     getIt.registerSingleton<HttpClient>(
       HttpClientImplementation(),
